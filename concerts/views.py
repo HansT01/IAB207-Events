1e0fb3e28c79f1f2238562e15624b25ca2c6fb6d
Hans Teh <n10530851@qut.edu.au>
2021-10-20 04:11:01 +1000

    Implemented findevents functionality and navigation
    Created sample event function call in views.

--- concerts/views.py
+++ concerts/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from .models import Event, Comment
 
 mainbp = Blueprint("main", __name__)
 
@@ -6,3 +7,35 @@ mainbp = Blueprint("main", __name__)
 @mainbp.route("/")
 def index():
     return render_template("pages/home.html")
+
+
+@mainbp.route("/findevents")
+def findevents():
+    events = [sample_event()]
+    return render_template("pages/findevents.html", events=events)
+
+
+def sample_event():
+    title = "EVENT TITLE"
+    artist = "ARTIST NAME"
+    genre = "EVENT GENRE"
+    datetime = "EVENT DATETIME"
+    venue = "EVENT VENUE"
+    desc = "EVENT DESCRIPTION"
+    status = "EVENT STATUS"
+    image = "EVENT IMAGE"
+    tickets = 200
+    price = 25
+
+    event = Event(
+        title, artist, genre, datetime, venue, desc, status, image, tickets, price
+    )
+
+    comment_user = "USERNAME"
+    comment_desc = "COMMENT DESCRIPTION"
+    comment_time = "3:19 AM"
+
+    comment = Comment(comment_user, comment_desc, comment_time)
+    event.add_comment(comment)
+
+    return event
