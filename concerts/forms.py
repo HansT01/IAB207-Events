0e316ac6dfa627767a44eedcedca0b312dff6894
Hans Teh <n10530851@qut.edu.au>
2021-10-20 06:06:27 +1000

    Created flask forms for login and register

---
+++ concerts/forms.py
@@ -0,0 +1,23 @@
+from flask_wtf import FlaskForm
+from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField
+from wtforms.validators import InputRequired, Email, EqualTo
+
+
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[InputRequired("Enter an email")])
+    password = PasswordField("Password", validators=[InputRequired("Enter a password")])
+    submit = SubmitField("Login")
+
+
+class RegisterForm(FlaskForm):
+    email = StringField("Email", validators=[Email("Please enter a valid email")])
+    username = StringField("Username", validators=[InputRequired("Enter a username")])
+    password = PasswordField("Password", validators=[InputRequired("Enter a password")])
+    confirm = PasswordField(
+        "Confirm password",
+        validators=[
+            InputRequired("Enter the password again"),
+            EqualTo("confirm", message="Passwords should match"),
+        ],
+    )
+    submit = SubmitField("Register")
