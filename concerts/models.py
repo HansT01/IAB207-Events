58cf23bb8453e4ed3336fbbf3311ef38b8f07a39
Hans Teh <n10530851@qut.edu.au>
2021-10-20 03:42:11 +1000

    Created models.py

---
+++ concerts/models.py
@@ -0,0 +1,25 @@
+class Event:
+    def __init__(self, title, artist, genre, datetime, venue, desc, status):
+        self.title = title
+        self.artist = artist
+        self.genre = genre
+        self.datetime = datetime
+        self.venue = venue
+        self.desc = desc
+        self.status = status
+
+        self.comments = list()
+
+    def add_comment(self, comment):
+        self.comments.append(comment)
+
+
+class Comment:
+    def __init__(self, user, desc, created_at):
+        self.user = user
+        self.desc = desc
+        self.created_at = created_at
+
+    def __repr__(self):
+        str = ("User: {0}\n" "Description: {1}\n").format(self.user, self.desc)
+        return str
