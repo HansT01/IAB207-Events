d122bea42b6e3b512a5aa00dc9dde8dad2882f6e
Hans Teh <n10530851@qut.edu.au>
2021-10-20 13:44:04 +1000

    Created EventForm in forms.py

--- concerts/forms.py
+++ concerts/forms.py
@@ -1,6 +1,16 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileAllowed
 from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField
-from wtforms.validators import InputRequired, Email, EqualTo
+from wtforms.fields import DateTimeField, FloatField, IntegerField, SelectField
+from wtforms.fields import FileField
+from wtforms.validators import InputRequired, Email, EqualTo, NumberRange
+
+IMAGE_FILE_FORMATS = {"png", "jpg", "jpeg"}
+IMAGE_FILE_FORMATS_MSG = "Only supports file formats: {0}".format(
+    ", ".join(map(str, IMAGE_FILE_FORMATS))
+)
+
+EVENT_STATUS = {"upcoming", "inactive", "booked", "cancelled"}
 
 
 class LoginForm(FlaskForm):
@@ -11,13 +21,50 @@ class LoginForm(FlaskForm):
 
 class RegisterForm(FlaskForm):
     username = StringField("Username", validators=[InputRequired("Enter a username")])
-    email = StringField("Email", validators=[Email("Please enter a valid email")])
+    email = StringField("Email", validators=[Email("Enter a valid email")])
     password = PasswordField("Password", validators=[InputRequired("Enter a password")])
     confirm = PasswordField(
         "Confirm password",
         validators=[
             InputRequired("Enter the password again"),
-            EqualTo("confirm", message="Passwords should match"),
+            EqualTo("password", message="Passwords should match"),
         ],
     )
     submit = SubmitField("Register")
+
+
+class EventForm(FlaskForm):
+    title = StringField("Event title", validators=[InputRequired("Enter a title")])
+    artist = StringField(
+        "Artist name", validators=[InputRequired("Enter the name of the artist")]
+    )
+    genre = StringField("Genre", validators=[InputRequired("Enter the event genre")])
+    datetime = DateTimeField(
+        "Date and time", validators=[InputRequired("Enter a title")]
+    )
+    venue = StringField(
+        "Venue address", validators=[InputRequired("Enter the venue address")]
+    )
+    desc = TextAreaField(
+        "Event title", validators=[InputRequired("Enter a description")]
+    )
+    status = SelectField(
+        "Event title",
+        choices=EVENT_STATUS,
+        validators=[InputRequired("Select a status")],
+    )
+    image = FileField(
+        "Event title",
+        validators=[
+            InputRequired("Image cannot be empty"),
+            FileAllowed(IMAGE_FILE_FORMATS, message=IMAGE_FILE_FORMATS_MSG),
+        ],
+    )
+    tickets = IntegerField(
+        "Available tickets",
+        validators=[NumberRange(min=0, message="Enter a valid number of tickets")],
+    )
+    price = FloatField(
+        "Ticket price", validators=[NumberRange(min=0, message="Enter a valid price")]
+    )
+    submit = SubmitField("Save")
