5aec6320130f05bb24e2334a4bedb24ce69204ec
Hans Teh <n10530851@qut.edu.au>
2021-11-02 13:03:10 +1000

    Testing DATABASE_URL

--- concerts/models.py
+++ concerts/models.py
@@ -6,11 +6,14 @@ from . import db
 
 
 def setup_db(app):
-    database_name = "local_db_name"
-    default_database_path = "postgresql://{}:{}@{}/{}".format(
-        "postgres", "password", "localhost:5432", database_name
-    )
-    database_path = os.getenv("DATABASE_URL", default_database_path)
+    # database_name = "local_db_name"
+    # default_database_path = "postgresql://{}:{}@{}/{}".format(
+    #     "postgres", "password", "localhost:5432", database_name
+    # )
+    print("here1")
+    database_path = os.getenv("DATABASE_URL")
+    print(database_path)
+    print("here2")
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
